a1951657fcb814a119d513823a1d87c36fb97963
DMZCON International Conference on Cyber Security <138613531+dmzcon@users.noreply.github.com>
2023-08-15 19:43:08 +0200

    Create lesson-001.cpp

---
+++ fast-start-in-C/lesson-001.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+  printf("Hello World\n");
+  return 0;
+}
+
+///////////////////////////////////////////////
+//   THIS IS SIMPLE PROGRAM ON C
+//   The C language is a low-level language: it means that you are very near to hardware
+//   To run this program you need some C-language Compiler, for example, GCC
+
+//   Use the following command to compile this program:
+//      gcc ./lesson-001.cpp -o lesson-001.executable
+
+//   Look at files in the directory: you will be able to find new file: "lesson-001.executable"
+//   Now launch this file to execute the program
+//     ./lesson-001.executable
+///////////////////////////////////////////////
